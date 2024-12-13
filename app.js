963e283a8265c1957403af64ebfeb8b681390fbd
CannyRo <whatcannydev@gmail.com>
2024-12-13 15:12:25 +0100

    create app.js + draft of sequence

---
+++ app.js
@@ -0,0 +1,22 @@
+(function app() {
+    // variables
+    // ...
+    // functions
+    // ...
+
+    // initialization
+
+    // check location
+
+    // check cookie
+
+    // get data by id
+
+    // save data
+
+    // show data
+
+    // listener/observer
+
+    // sequence get data + save data + show data
+})();
